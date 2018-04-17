0ab427d5c6abf7d1fad7997361654316d3a5d448
d-savkin <d.savkin@qiwi.com>
2018-04-17 17:29:58 +0300

    add redux dev tools

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
 
+import {BrowserRouter, Route} from 'react-router-dom';
+
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+const dev = process.env.NODE_ENV === 'development';
+
+const store = createStore(
+  () => {},
+  null,
+  dev ?
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() :
+    () => () => {}
+);
+
 class App extends Component {
   render() {
     return (
-      <div />
+      <Provider store={store}>
+        <BrowserRouter>
+          <div>
+            <Route exact path="/" component={() => <div></div>} />
+            <Route path="/blog" component={() => <div></div>} />
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
