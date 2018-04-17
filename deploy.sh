5b559147d79f50a074113e69c95a4baf49cd46bc
d-savkin <d.savkin@qiwi.com>
2018-04-17 16:35:55 +0300

    fix forever

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ yarn build
 rm -rf /var/www/build_old/
 mv /var/www/build/ /var/www/build_old/
 cp -r ./build/ /var/www/
-forever stop /root/city-del/src/api/server.js
-forever start /root/city-del/src/api/server.js
+forever stop /root/privateblog/src/api/server.js
+forever start /root/privateblog/src/api/server.js
