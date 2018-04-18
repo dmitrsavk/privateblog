1af6f570e92f83e5cac27b2b92b4d04fa24a20b7
d-savkin <d.savkin@qiwi.com>
2018-04-18 19:08:56 +0300

    add redirect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 
-import { Route } from 'react-router-dom';
+import { Route, Redirect, Switch } from 'react-router-dom';
 import Landing from './components/Landing/Landing';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Route exact path="/" component={Landing} />
-        <Route path="/blog" component={() => <div></div>} />
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route path="/blog" component={() => <div></div>} />
+          <Redirect to="/" />
+        </Switch>
       </div>
     );
   }
