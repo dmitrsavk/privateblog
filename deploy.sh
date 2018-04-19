146cf229a809664365a453d632c888f2629267d8
d-savkin <d.savkin@qiwi.com>
2018-04-19 12:49:37 +0300

    fix

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,3 @@ yarn build
 rm -rf /var/www/build_old/
 mv /var/www/build/ /var/www/build_old/
 cp -r ./build/ /var/www/
-forever stop /root/privateblog/src/api/server.js
-forever start /root/privateblog/src/api/server.js
