95164d61f35c43625340de415a7084c6aa8b6ec7
d-savkin <d.savkin@qiwi.com>
2018-04-26 17:29:59 +0300

    fix url

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 
 import { login } from '../../state/auth/reducer';
 
-const url = 'https://www.facebook.com/v2.12/dialog/oauth?client_id=159008188111833&redirect_uri=https://privateblog.ru/';
+const url = 'https://www.facebook.com/v2.12/dialog/oauth?client_id=159008188111833&redirect_uri=https://privateblog.ru/api/auth/fb';
 
 class Landing extends Component {
   render() {
